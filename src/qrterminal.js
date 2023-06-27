65dc5cd045e2d81051a8bdba39e878b5699e4d43
ivan <ivanzigonipires.11@gmail.com>
2023-06-27 15:42:44 -0300

    os arch testing

--- src/qrterminal.js
+++ src/qrterminal.js
@@ -1,15 +1,19 @@
 const q = require("qrcode-terminal");
+const os = require("os");
 
 function main() {
 
-    const string =
-        "2@FkFXNsEfBlI6kYHkVAfuzQXpBReDCjRStQXWv+VI0vZgDhVzPex/SJyRpu8SPplIdWv1bFdyeHO8xA==,6dT94urJkr0tTVQ9iFpWj0KgjbtePUL9yHjFKFTY8A4=,NmQI8KWGnQAeCsPzPz45/7tMy5MbtDOd2m9v08LmFU4=,PYdgKwQ3Gm1DDTXE7V0fZpVoFOeIt/fy37tymkSb7mI=,1"
+    // const string =
+    //     "2@FkFXNsEfBlI6kYHkVAfuzQXpBReDCjRStQXWv+VI0vZgDhVzPex/SJyRpu8SPplIdWv1bFdyeHO8xA==,6dT94urJkr0tTVQ9iFpWj0KgjbtePUL9yHjFKFTY8A4=,NmQI8KWGnQAeCsPzPz45/7tMy5MbtDOd2m9v08LmFU4=,PYdgKwQ3Gm1DDTXE7V0fZpVoFOeIt/fy37tymkSb7mI=,1"
+    //
+    // q.generate(
+    //     string,
+    //     { small: true }
+    // )
 
-    q.generate(
-        string,
-        { small: true }
-    )
+    console.log(os.arch())
 
 }
 
 // main();
+
